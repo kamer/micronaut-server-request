c1b7f4afc1584f4d3a89ecec5ccb27b482e17327
kamer <kamer@kamer.dev>
2024-03-29 16:18:47 +0300

    Commit

--- src/main/java/example/micronaut/DemoController.java
+++ src/main/java/example/micronaut/DemoController.java
@@ -3,16 +3,21 @@ package example.micronaut;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.print.attribute.standard.Media;
 
 @Controller("/demo")
 public class DemoController {
 
+	private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
 	@Post
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public HttpResponse<User> demo(@Body User user) {
-		return HttpResponse.ok(user);
+		log.info("User {} received.", user.fullName);
+		return HttpResponse.ok();
 	}
 }
--- src/main/java/example/micronaut/User.java
+++ src/main/java/example/micronaut/User.java
@@ -5,25 +5,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.context.ServerRequestContext;
+import io.micronaut.serde.annotation.Serdeable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
 import java.util.Optional;
 
+@Serdeable
 @Introspected
-public record User(String fullName) {
+public class User {
 
 	private static final Logger log = LoggerFactory.getLogger(User.class);
 
-	@JsonCreator
-	public User(@JsonProperty("name") String name, @JsonProperty("surname") String surname) {
-		this(name + " " + surname);
+	String fullName;
+
+	public User(@JsonProperty("name") String name, @JsonProperty("surname") String surname, @JsonProperty("attributes") Map<String, String> attributes) {
+		this.fullName = name + " " + surname;
 
 		final Optional<HttpRequest<Object>> httpRequest = ServerRequestContext.currentRequest();
 		if (httpRequest.isEmpty()) {
-			throw new RuntimeException("HttpRequest empty");
+			log.error("HttpRequest empty");
 		} else {
-			log.info("Request is present");
+			log.info("HttpRequest present");
 		}
 	}
 }
